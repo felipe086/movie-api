40799ed89536d8556d6e40e340e946181e935cfb
Felipe <felipegalegoosawa@gmail.com>
2022-04-07 22:38:41 -0300

    upgrade to react 18, globalstyles import

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from "react"
-import ReactDOM from "react-dom"
-import "./global.css"
-import Home from "./pages/Home"
+import ReactDOM from "react-dom/client"
+import GlobalStyles from "./global"
+import Home from "./pages/components/Home"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Details from "./pages/Details"
+import Details from "./pages/components/Details"
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/details/:id" element={<Details />} />
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById("root")
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/details/:id" element={<Details />} />
+//     </Routes>
+//   </BrowserRouter>,
+//   document.getElementById("root")
+// )
+
+const container = document.getElementById("root")
+const root = ReactDOM.createRoot(container)
+root.render(
+  <>
+    <GlobalStyles />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details/:id" element={<Details />} />
+      </Routes>
+    </BrowserRouter>
+  </>
 )
